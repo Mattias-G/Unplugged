b57865b109f4d4abdaf3a1916f595734447b22f4
Sebastian Hjelm <melon.bag@hotmail.com>
2017-07-29 20:10:38 +0200

    Improved the sockets and plugs.

---
+++ Unplugged/Assets/Scripts/Plug.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plug : MonoBehaviour {
+
+	private Socket connectedSocket;
+	
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		var socket = collider.gameObject.GetComponent<Socket>();
+		if (socket)
+		{
+			if (!connectedSocket)
+			{
+				var angleDiff = Quaternion.Angle(socket.gameObject.transform.rotation, gameObject.transform.rotation);
+
+				if (Mathf.Abs(angleDiff) < 70)  // How big an interval should we allow?
+				{
+					var body = GetComponent<Rigidbody2D>();
+					connectedSocket = socket;
+					socket.connect(this);
+					body.velocity = Vector2.zero;
+					body.bodyType = RigidbodyType2D.Static;
+				}
+			}
+		}
+	}
+}
---
+++ Unplugged/Assets/Scripts/Socket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Socket : MonoBehaviour {
+
+	private Plug connectedPlug;
+
+	public void connect(Plug plug)
+	{
+		connectedPlug = plug;
+	}
+
+	public void disconnect()
+	{
+		connectedPlug = null;
+	}
+}
