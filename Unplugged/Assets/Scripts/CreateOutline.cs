4a6dc32242c27e29fb7bbacebe436e3e90320832
Mattias Gustafsson <dat13mgu@student.lu.se>
2017-07-30 19:51:33 +0200

    Added outlines to ground and boxes

---
+++ Unplugged/Assets/Scripts/CreateOutline.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateOutline : MonoBehaviour {
+
+	void Start () {
+		var outline = transform.GetChild(0);
+		outline.localScale = new Vector3(1 + 0.04f / transform.localScale.x,
+			1 + 0.04f / transform.localScale.y,
+			1 + 0.04f / transform.localScale.z);
+	}
+}
