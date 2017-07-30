13464d2a303fed20c3187cdee83de88fd8b285af
Sebastian Hjelm <dat13shj@student.lu.se>
2017-07-30 11:45:54 +0200

    Added a goal area and fade in/out scripts

---
+++ Unplugged/Assets/Scripts/SceneFadeIn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneFadeIn : MonoBehaviour
+{	
+	public Color fadeColor;
+	public float fadeTime = 2f;
+	
+	private Image fadingOverlay;
+	private float fadeCounter;
+	
+	void Start ()
+	{
+		fadingOverlay = GetComponentInChildren<Image>();
+		fadeCounter = fadeTime;
+		transform.position = Vector2.zero;
+	}
+
+	void Update()
+	{
+		fadeCounter -= Time.deltaTime;
+		if (fadeCounter <= 0)
+		{
+			fadeCounter = 0;
+			Destroy(gameObject);
+		}
+
+		float alpha = fadeCounter / fadeTime;
+		fadingOverlay.color = new Color(fadeColor.r, fadeColor.g, fadeColor.b, alpha);
+	}
+}
---
+++ Unplugged/Assets/Scripts/SceneFadeOut.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneFadeOut : MonoBehaviour
+{
+	
+	public Color fadeColor;
+	public float fadeTime = 2f;
+	public float blackTime = 1f;
+	public string targetScene;
+	public bool waitForDialog = true;
+	
+	private Image fadingOverlay;
+	private float fadeCounter;
+	private float blackCounter;
+
+	void Awake()
+	{
+		fadingOverlay = GetComponentInChildren<Image>();
+		transform.position = Vector2.zero;
+		DontDestroyOnLoad(gameObject);
+		SceneManager.sceneLoaded += OnSceneWasLoaded;
+	}
+	
+	protected virtual void Start ()
+	{
+		StartCoroutine(FadeAndLoadScene());
+	}
+
+	private IEnumerator FadeAndLoadScene()
+	{
+		yield return new WaitUntil(HasFinishedFading);
+		yield return new WaitWhile(() => !CanSwitchScene());
+		SceneManager.LoadScene(targetScene);
+	}
+
+	protected virtual bool CanSwitchScene()
+	{
+		return true; //!(waitForDialog && dialog.HasDialog());
+	}
+
+	public bool HasFinishedFading()
+	{
+		return fadeCounter >= fadeTime && blackCounter >= blackTime;
+	}
+
+	public virtual void Update()
+	{
+		fadeCounter += Time.deltaTime;
+		fadeCounter = Mathf.Clamp(fadeCounter, 0, fadeTime);
+
+		if (fadeCounter == fadeTime)
+			blackCounter += Time.deltaTime;
+
+		float alpha = fadeCounter / fadeTime;
+		fadingOverlay.color = new Color(fadeColor.r, fadeColor.g, fadeColor.b, alpha);
+	}
+
+	protected virtual void OnSceneWasLoaded(Scene scene, LoadSceneMode mode)
+	{
+		var inFader = GameObject.FindObjectOfType<SceneFadeIn>();
+		if (inFader)
+			inFader.fadeColor = fadeColor;
+		SceneManager.sceneLoaded -= OnSceneWasLoaded;
+		Destroy(gameObject);
+	}
+}
