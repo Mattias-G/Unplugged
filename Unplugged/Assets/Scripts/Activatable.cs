ab7e564bdd7f538227b30f5981e935c34184b5a9
Sebastian Hjelm <dat13shj@student.lu.se>
2017-07-30 15:00:51 +0200

    Added Activator/Activatable scripts and made it possible for sockets to activate stuff

---
+++ Unplugged/Assets/Scripts/Activatable.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public abstract class Activatable : MonoBehaviour {
+
+	public int activationsRequired = 1;
+	public bool inverted = false;
+
+	private HashSet<GameObject> poweringSources = new HashSet<GameObject>();
+
+	public virtual void Start() {
+		UpdateActive();
+	}
+
+	public bool IsActive() {
+		return (poweringSources.Count >= activationsRequired) ^ inverted; // ^ is xor
+	}
+
+	public void Activate(GameObject source) {
+		bool changed = poweringSources.Add(source);
+		if (changed && poweringSources.Count == activationsRequired) {
+			UpdateActive();
+		}
+	}
+
+	public void Deactivate(GameObject source) {
+		bool changed = poweringSources.Remove(source);
+		if (changed && poweringSources.Count == activationsRequired - 1) {
+			UpdateActive();
+		}
+	}
+
+	private void UpdateActive() {
+		if (IsActive()) {
+			OnActivate();
+		} else {
+			OnDeactivate();
+		}
+	}
+
+	protected virtual void OnActivate() { }
+	protected virtual void OnDeactivate() { }
+
+	public int GetActivationCount() {
+		return poweringSources.Count;
+	}
+
+	public virtual void OnDrawGizmosSelected() {
+		Gizmos.color = new Color(1f, 0.5f, 0f);
+
+		var activators = GameObject.FindObjectsOfType<Activator>();
+		DrawArrowsFromReferences<Activator, Activatable>(activators, a => a.activatables, a => a);
+	}
+
+	protected void DrawArrowsFromReferences<T1, T2>(T1[] objects, Func<T1, T2[]> getActivatables, Func<T2, Activatable> getActivatable) where T1 : MonoBehaviour {
+		foreach (var o in objects) {
+
+			if (Array.Exists(getActivatables.Invoke(o), a => getActivatable.Invoke(a) == this)) {
+				Gizmos.DrawLine(transform.position, o.transform.position);
+				DrawArrowBetween(o.transform.position, transform.position);
+			}
+		}
+	}
+
+	protected void DrawArrowBetween(Vector3 A, Vector3 B) {
+		var difference = B - A;
+		var direction = Mathf.Atan2(difference.y, difference.x);
+
+		var center = (A + B) / 2;
+		GizmosExtensions.DrawArrow(center, direction);
+	}
+}
--- Unplugged/Assets/Scripts/Socket.cs
+++ Unplugged/Assets/Scripts/Socket.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Socket : MonoBehaviour {
+public class Socket : Activator {
 
 	private Plug connectedPlug;
 	private float cooldown;
 
-	private void Update() {
+	private void Update()
+	{
 		if (cooldown > 0)
 			cooldown -= Time.deltaTime;
 	}
@@ -15,15 +16,18 @@ public class Socket : MonoBehaviour {
 	public void Connect(Plug plug)
 	{
 		connectedPlug = plug;
+		ActivateObjects();
 	}
 
 	public void Disconnect()
 	{
 		connectedPlug = null;
 		cooldown = .5f;
+		DeactivateObjects();
 	}
 
-	public bool CanConnect() {
+	public bool CanConnect()
+	{
 		return cooldown <= 0;
 	}
 }
