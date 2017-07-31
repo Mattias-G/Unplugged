a081d24dcfe11eef4cd6be3fb6ff1cb2d9a4fba3
Mattias Gustafsson <dat13mgu@student.lu.se>
2017-07-31 14:07:40 +0200

    Level ends when goal is activated

---
+++ Unplugged/Assets/Scripts/LevelEnder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnder : MonoBehaviour {
+
+	public string NextLevel;
+	public GameObject faderPrefab;
+	public float fadeDuration = 2f;
+
+
+	public void EndLevel()
+	{
+		var faderObject = (GameObject)Instantiate(faderPrefab, Vector3.zero, Quaternion.identity);
+		var sceneFader = faderObject.GetComponent<SceneFadeOut>();
+		sceneFader.targetScene = NextLevel;
+		sceneFader.fadeTime = fadeDuration;
+	}
+}
