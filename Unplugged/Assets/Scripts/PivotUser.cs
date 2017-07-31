22ebf1840ed8a33c8c9de44601fe61e7dbb79b58
Sebastian Hjelm <dat13shj@student.lu.se>
2017-07-31 11:04:23 +0200

    Added trapdoors

---
+++ Unplugged/Assets/Scripts/PivotUser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotUser : Activatable {
+	protected Transform pivot;
+
+	public override void Start() {
+		base.Start();
+
+		pivot = transform.GetChild(0);
+		var list = new Transform[transform.childCount - 1];
+		for (int i = 0; i < list.Length; i++) {
+			list[i] = transform.GetChild(i + 1);
+		}
+
+		transform.DetachChildren();
+		for (int i = 0; i < list.Length; i++) {
+			list[i].SetParent(transform);
+		}
+
+		transform.SetParent(pivot, true);
+	}
+}
---
+++ Unplugged/Assets/Scripts/Trapdoor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trapdoor : PivotUser
+{
+	public float goalAngle;
+	public float timeToOpen = 1;
+
+	private bool activated;
+	private float timer;
+	private float startAngle;
+
+	public override void Start()
+	{
+		startAngle = transform.rotation.eulerAngles.z;
+		goalAngle = startAngle + goalAngle;
+		base.Start();
+	}
+
+	void FixedUpdate()
+	{
+		if (activated && timer < timeToOpen)
+		{
+			timer += Time.fixedDeltaTime;
+			if (timer >= timeToOpen) timer = timeToOpen;
+		}
+		if (!activated && timer > 0)
+		{
+			timer -= Time.fixedDeltaTime;
+			if (timer < 0) timer = 0;
+		}
+
+		var angle = (goalAngle - startAngle) * timer / timeToOpen + startAngle;
+		pivot.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+
+
+	protected override void OnActivate()
+	{
+		activated = true;
+	}
+
+	protected override void OnDeactivate()
+	{
+		activated = false;
+	}
+}
