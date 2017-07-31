d5a51b0ea802121617af9c74d9c934351dd6bee7
Mattias Gustafsson <dat13mgu@student.lu.se>
2017-07-31 13:27:26 +0200

    Added a goal structure

---
+++ Unplugged/Assets/Scripts/AnimationStarter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStarter : Activatable {
+
+	public string animationName = "";
+
+	protected override void OnActivate() {
+		GetComponent<Animator>().enabled = true;
+	}
+}
