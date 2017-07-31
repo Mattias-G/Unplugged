b40c6f7924b0f6125fc7dcbf2a0ef417beff7468
Sebastian Hjelm <dat13shj@student.lu.se>
2017-08-01 01:21:14 +0200

    Added some keyboard shortcuts

---
+++ Unplugged/Assets/GlobalKeys.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalKeys : MonoBehaviour {
+
+	public GameObject faderPrefab;
+	
+	void Update ()
+	{
+		if (Input.GetButton("Restart")) {
+			string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+			var faderObject = (GameObject)Instantiate(faderPrefab, Vector3.zero, Quaternion.identity);
+			var sceneFader = faderObject.GetComponent<SceneFadeOut>();
+			sceneFader.blackTime = 0;
+			sceneFader.targetScene = currentScene;
+		} else if (Input.GetButton("Quit")) {
+			Application.Quit();
+		}
+	}
+}
---
+++ Unplugged/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer instance = null;
+	public static MusicPlayer Instance
+	{
+		get { return instance; }
+	}
+
+	void Start()
+	{
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
