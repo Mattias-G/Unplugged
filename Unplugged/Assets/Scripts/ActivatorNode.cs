7463a88aeeed93abab858ab1d6d93fc779719220
Sebastian Hjelm <dat13shj@student.lu.se>
2017-07-31 22:04:32 +0200

    Implemented activator nodes

--- Unplugged/Assets/Scripts/ActivatorNode.cs
+++ Unplugged/Assets/Scripts/ActivatorNode.cs
@@ -3,4 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActivatorNode : Activator {
+
+	protected override void OnActivate()
+	{
+		ActivateObjects();
+	}
+
+	protected override void OnDeactivate()
+	{
+		DeactivateObjects();
+	}
 }
