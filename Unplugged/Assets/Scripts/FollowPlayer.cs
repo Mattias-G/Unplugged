3da9d84fa5e01a2e4123b0880bf4691fd1738b6c
Markus-O <markus.iluvatar@gmail.com>
2017-07-31 11:03:36 +0200

    Camera now follows player

---
+++ Unplugged/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	private GameObject player;
+
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	void LateUpdate ()
+	{
+		var camera = Camera.main;
+		var position = player.transform.position;
+		position.z = camera.transform.position.z;
+		camera.transform.position = position;
+	}
+}
